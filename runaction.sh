a2cb59f1620ce9e4c00dff6cecba8527fa46945b
Nolan McCafferty <njma2016@pomona.edu>
2020-05-27 16:03:43 -0700

    remove echos

--- runaction.sh
+++ runaction.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 
-echo "these are the files:"
-
-ls -a
-
-ls -a ../
-ls -a ../../ 
-
-echo "those were the files"
-cd ../../
-
 error=0
 find . -name '*.md' -exec proselint {} + || error=1
 
-echo $error
 exit $error
